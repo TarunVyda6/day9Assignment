224c9ee0a8a65df4cc220a9f3cfd4c27f61e8892
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-09-07 16:59:05 +0530

    added new feature of switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -16,14 +16,22 @@ fi
 
 wagePerHour=20
 fullDayHour=8
+isFullTime=0
+isPartTime=1
 
-dailyEmployeeWage=$(( $wagePerHour*$fullDayHour ))
+echo "if employee is full time then press 0"
+echo "if employee is part time then press 1"
 
-echo "daily employee wage is : "$dailyEmployeeWage
+read choice
 
+case $choice in
+	0)	dailyEmployeeWage=$(( $wagePerHour*$fullDayHour ))
+		echo "daily employee wage is : "$dailyEmployeeWage
+		;;
+	1)	partTimeHour=$(( $fullDayHour/2 ))
+		dailyEmployeeWage=$(( $wagePerHour*$partTimeHour ))
+		echo "daily employee wage for part time is : "$dailyEmployeeWage
+		;;
+	*)	echo "invalid choice"
 
-partTimeHour=$(( $fullDayHour/2 ))
-
-dailyEmployeeWage=$(( $wagePerHour*$partTimeHour ))
-
-echo "daily employee wage for part time is : "$dailyEmployeeWage
+esac
