79bd32a531071666e5599fdde805e0b2b330b9d9
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-09-07 18:33:36 +0530

    adding EmployeeTotalWage till it reach hours or months

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -19,6 +19,7 @@ fullDayHour=8
 isFullTime=0
 isPartTime=1
 workingDaysPerMonth=20
+workingHoursPerMonth=100
 
 echo "if employee is full time then press 0"
 echo "if employee is part time then press 1"
@@ -27,13 +28,27 @@ read choice
 
 case $choice in
 	0)	dailyEmployeeWage=$(( $wagePerHour*$fullDayHour ))
+		timeHour=$fullDayHour
 		;;
-	1)	partTimeHour=$(( $fullDayHour/2 ))
-		dailyEmployeeWage=$(( $wagePerHour*$partTimeHour ))
+	1)	timeHour=$(( $fullDayHour/2 ))
+		dailyEmployeeWage=$(( $wagePerHour*$timeHour ))
 		;;
 	*)	echo "invalid choice"
 
 esac
+day=0
+hour=0
+totalEmployeeWage=0
+while(( $day<$workingDaysPerMonth && $hour<$workingHoursPerMonth ))
+do
+	if(( $hour >= 96 ))
+	then
+		totalEmployeeWage=$(( $totalEmployeeWage+80 ))
+	else
+		totalEmployeeWage=$(( $totalEmployeeWage+$dailyEmployeeWage ))
+	fi
+	hour=$(( $hour+$timeHour ))
+	day=$(( $day+1 ))
+done
 
-monthlyEmployeeWage=$(( $dailyEmployeeWage*$workingDaysPerMonth ))
-echo "monthly employee wage is : "$monthlyEmployeeWage
+echo "total employee wage is : "$totalEmployeeWage
